714e4b1f51da72d8799179ccf65f6becf9dd5df3
JoseChan <chenyu01@linghit.com>
2018-04-30 22:19:05 +0800

    fix

--- src/Console/MatchConsole.php
+++ src/Console/MatchConsole.php
@@ -28,53 +28,49 @@ class MatchConsole extends Command
 
         $league = database()->select("league", ["id"]);
 
+        $time = time();
+        $match_data = [];
         foreach ($league as $v)
         {
             $res = Match::get(null, $league['id']);
 
             $match_data = [];
             foreach ($res['match'] as $match) {
+                list($home_gb,$home_big,$home_en,$home_id) = explode(",",$match['h']);
+                list($away_gb,$away_big,$away_en,$away_id) = explode(",",$match['i']);
+                list($league_gb,$league_big,$league_en,$league_id,$is_simple) = explode(",",$match['c']);
 
+                $match_data[] = [
+                    "id" => $match["a"],
+                    "create_time" => $time,
+                    "status" => $match["f"],
+                    "start_time" => $match["d"],
+                    "home_id" => $home_id,
+                    "away_id" => $away_id,
+                    "league_id" => $league_id,
+                    "home_score" => $match["j"],
+                    "away_score" => $match["k"],
+                    "home_half_score" => $match["l"],
+                    "away_half_score" => $match["m"],
+                    "home_red" => $match["n"],
+                    "away_red" => $match["o"],
+                    "home_yellow" => $match["o"],
+                    "away_yellow" => $match["o"],
+                    "home_rank" => $match["p"],
+                    "away_rank" => $match["q"],
+                    "match_desc" => $match["r"],
+                    "round" => $match["s"],
+                    "area" => $match["t"],
+                    "weather_id" => $match["v"],
+                    "temperature" => $match["w"],
+                    "season" => $match["x"],
+                    "group" => $match["y"],
+                    "is_neutral" => $match["z"],
+                    "sub_id" => $match["subID"]
+                ];
             }
         }
 
-
-
-        $league_data = [];
-        $country_data = [];
-        $time = time();
-        foreach ($res['match'] as $v)
-        {
-
-            $league_data[] = [
-                "id" => "",
-                "create_time" => $time,
-                "type" => $v['type'],
-                "color" => $v['color'],
-                "gb" => $v['gb'],
-                "big" => $v['big'],
-                "en" => $v['en'],
-                "gb_short" => $v['gb_short'],
-                "big_short" => $v['big_short'],
-                "en_short" => $v['en_short'],
-                "current_season" => $v['Curr_matchSeason'],
-                "current_round" => $v['curr_round'],
-                "sum_round" => $v['sum_round'],
-                "country_id" => $v['countryID'],
-                "area_id" => $v['areaID'],
-                "sub_sclass" => isset($v['subSclass'])?$v['subSclass']:"",
-                "logo" => $v['logo'],
-            ];
-
-            $country_data[$v['countryID']] = [
-                "id" => $v['countryID'],
-                "country" => $v['country'],
-                "en" => $v['countryEn'],
-                "logo" => $v['countryLogo']
-            ];
-        }
-
-        database()->insert("league", $league_data);
-        database()->insert("country", $country_data);
+        database()->insert("match", $match_data);
     }
 }

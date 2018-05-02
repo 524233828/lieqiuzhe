ce8a181e68fcbf8b03310c54cf89e037d2458e54
JoseChan <chenyu01@linghit.com>
2018-05-02 10:40:21 +0800

    fix

---
+++ src/Console/TeamConsole.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenyu
+ * Date: 2018/4/28
+ * Time: 14:28
+ */
+
+namespace Console;
+
+
+use Qiutan\Team;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class TeamConsole extends Command
+{
+
+    public function configure()
+    {
+        $this->setName('fetch_team')
+            ->setDescription('获取球队信息');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+
+        Team::$redis = redis();
+        $res = Team::get();
+
+        $league_data = [];
+        $country_data = [];
+        $time = time();
+        foreach ($res['i'] as $v)
+        {
+
+            $team_data[] = [
+                "id" => $v['id'],
+                "create_time" => $time,
+                "league_id" => $v['lsID'],
+                "gb" => $v['g'],
+                "big" => $v['b'],
+                "en" => $v['e'],
+                "found" => $v['Found'],
+                "area" => $v['Area'],
+                "gym" => $v['gym'],
+                "capacity" => $v['Capacity'],
+                "flag" => $v['Flag'],
+                "addr" => $v['addr'],
+                "url" => $v['URL'],
+                "master" => $v['master'],
+            ];
+        }
+
+        database()->insert("team", $team_data);
+    }
+}

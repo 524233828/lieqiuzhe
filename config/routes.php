392f99c510a7705c13628a2b092fd9580aac2755
JoseChan <chenyu01@linghit.com>
2018-05-03 11:55:37 +0800

    fix

--- config/routes.php
+++ config/routes.php
@@ -10,7 +10,7 @@ route()->group(["prefix"=>"/match","middleware" => "dispatch"], function(){
     route()->get("/list", 'MatchListController@fetchMatchList')->withAddMiddleware("login");;
     route()->post("/collect", 'MatchListController@collectionMatch')->withAddMiddleware("login");
     route()->delete("/collect", 'MatchListController@collectionMatchCancel')->withAddMiddleware("login");
-    route()->get("/league", 'MatchListController@fetchLeague');
+    route()->get("/league/list", 'MatchListController@fetchLeague');
 });
 
 route()->get(["prefix"=>"/login","middleware" => "dispatch"], 'LoginController@get');

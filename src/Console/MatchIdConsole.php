204a284d7c98b731bd1a184fd8fe5bea7167a6ce
JoseChan <chenyu01@linghit.com>
2018-05-07 17:05:11 +0800

    fix

--- src/Console/MatchIdConsole.php
+++ src/Console/MatchIdConsole.php
@@ -13,7 +13,13 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class MatchIdConsole
+class MatchIdConsole extends Command
 {
+
+    public function configure()
+    {
+        $this->setName('fetch_match_id')
+            ->setDescription('获取比赛信息');
+    }
 
 }

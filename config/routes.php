b39f3100e01f73c21bf16107f4ee2660a64f06ca
JoseChan <chenyu01@linghit.com>
2018-05-09 12:02:04 +0800

    fix

--- config/routes.php
+++ config/routes.php
@@ -18,3 +18,10 @@ route()->group(["prefix"=>"/match","middleware" => "dispatch"], function(){
 route()->group(["prefix"=>"/match_collect","middleware" => "dispatch"], function(){
     route()->get("/list", 'MatchCollectionController@fetchAction')->withAddMiddleware("login");
 });
+
+//微信客服消息接口
+route()->group("/wechat",function(){
+    route()->get("/customer","WechatController@wxapp");
+    route()->post("/customer","WechatController@wxapp");
+    route()->put("/customer","WechatController@wxapp");
+});
---
+++ src/Controller/WechatController.php
@@ -0,0 +1,48 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenyu
+ * Date: 2018/1/18
+ * Time: 9:33
+ */
+
+namespace Controller;
+
+
+use EasyWeChat\Foundation\Application;
+use FastD\Http\ServerRequest;
+use Wxapp\Wxapp;
+
+class WechatController
+{
+    public function wxapp(ServerRequest $request)
+    {
+        $log = myLog("wxappCustomer");
+        $params = $request->getQueryParams();
+        $open_id = $params['openid'];
+        $body = $request->getBody()->getContents();
+        $log->addDebug("params:".json_encode($params));
+        $log->addDebug("body:".$body);
+        $conf = config()->get("wxapp");
+        $app_id = $conf['app_id'];
+        $app_secret = $conf['app_secret'];
+        $wxapp = new Wxapp($app_id,$app_secret);
+
+        $data = [
+            "touser" => $open_id,
+            "msgtype" => "link",
+            "link" => [
+                "title" => "【稳稳收米】欢迎关注夜猫情报局！",
+                "description" => "夜猫情报局一直致力于给大家提供迅捷实用的足彩情报 分享足彩技巧干货，帮助大家更稳地收米",
+                "url" => "http://mp.weixin.qq.com/s/yfgZZzxVOfwI9a37O7-Uig",
+                "thumb_url" => "http://www.ym8800.com/upload/maozhentan.png"
+            ]
+        ];
+
+        $result = $wxapp->bindRedis(redis())->sendCustomerMsg($data);
+
+        $log->addDebug("Result:".$result);
+
+
+    }
+}

9b9c0f9992b87ac55b0cf4cd1df728cb6ba5ba47
JoseChan <chenyu01@linghit.com>
2018-05-16 17:45:11 +0800

    fix

---
+++ src/Console/MatchModifyConsole.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: chenyu
+ * Date: 2018/5/16
+ * Time: 17:36
+ */
+
+namespace Console;
+
+
+use Qiutan\Match;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class MatchModifyConsole extends Command
+{
+    public function configure()
+    {
+        $this->setName('match_modify')
+            ->setDescription('获取比赛更新信息');
+    }
+
+    public function execute(InputInterface $input, OutputInterface $output)
+    {
+        Match::$redis = redis();
+
+        $res = Match::modifyRecord();
+
+        var_dump($res);
+    }
+}

7230e385c82f0e5d496024f0d5ff0c9598b59599
JoseChan <chenyu01@linghit.com>
2018-05-17 15:55:57 +0800

    fi

--- src/Console/TestConsole.php
+++ src/Console/TestConsole.php
@@ -15,7 +15,7 @@ use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class LeagueConsole extends Command
+class TestConsole extends Command
 {
 
     public function configure()

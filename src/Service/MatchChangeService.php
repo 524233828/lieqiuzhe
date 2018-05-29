ab796c392092541db98ebcf065a1e267254199f1
JoseChan <chenyu01@linghit.com>
2018-05-29 18:28:12 +0800

    fix

--- src/Service/MatchChangeService.php
+++ src/Service/MatchChangeService.php
@@ -81,21 +81,21 @@ class MatchChangeService
             $where = ["id" => $match_id];
 
             //增加开始比赛状态改变，做推送
-            $count = MatchModel::count(["id" => $match_id, "status" => 0]);
-            if($count){
-                $ids[] = $match_id;
-            }
+//            $count = MatchModel::count(["id" => $match_id, "status" => 0]);
+//            if($count){
+//                $ids[] = $match_id;
+//            }
 
             $data = [
-                "status" => $status,
-                "home_score" => $home_score,
-                "away_score" => $away_score,
-                "home_half_score" => $home_half_score,
-                "away_half_score" => $away_half_score,
-                "home_red" => $home_red,
-                "away_red" => $away_red,
-                "home_yellow" => $home_yellow,
-                "away_yellow" => $away_yellow,
+//                "status" => $status,
+//                "home_score" => $home_score,
+//                "away_score" => $away_score,
+//                "home_half_score" => $home_half_score,
+//                "away_half_score" => $away_half_score,
+//                "home_red" => $home_red,
+//                "away_red" => $away_red,
+//                "home_yellow" => $home_yellow,
+//                "away_yellow" => $away_yellow,
                 "home_corner" => $home_corner,
                 "away_corner" => $away_corner,
                 "current_minutes" => $current_minutes,
@@ -107,85 +107,85 @@ class MatchChangeService
 
             }
         }
-        $log->addDebug("match_ids:".json_encode($ids));
-        //增加开始比赛状态改变，做推送
-        if(empty($ids)||count($ids)<1)
-        {
-            return false;
-        }
-
-        $collect = MatchCollectionModel::fetch("*", ["match_id"=>$ids]);
-
-        $where2['m.id'] = $ids;
-        $res = MatchModel::fetch(
-            $where2,
-            [
-                "m.id(match_id)",
-                "l.gb_short(league_name)",
-                "l.color(league_color)",
-                "m.start_time(match_time)",
-                "h.gb(home)",
-                "h.flag(home_flag)",
-                "a.gb(away)",
-                "a.flag(away_flag)",
-                "m.home_score",
-                "m.away_score",
-                "m.home_red",
-                "m.away_red",
-                "m.home_yellow",
-                "m.away_yellow",
-                "m.status",
-                "m.current_minutes",
-            ]
-        );
-
-        $match_index = [];
-        foreach ($res as $k => $v){
-            $res[$k]['match_time'] = date("Y-m-d H:i:s", $v['match_time']);
-            $res[$k]['is_collect'] = 0;
-            $match_index[$v['match_id']] = $res[$k];
-        }
-
-        $conf = config()->get("wxapp");
-        $wxapp = new Wxapp($conf['app_id'], $conf['app_secret']);
-        $template_id = "CqlL_4sZ3Axfcth7vFd0bREMM0ayWt4loscCk8hhnBQ";
-        foreach ($collect as $v){
-            $log->addDebug("用户：{$v['user_id']}，关注了{$v['match_id']}");
-            $user = UserModel::getUserInfo($v['user_id'], ["openid"]);
-            if(!empty($v['form_id'])){
-
-                $match = $match_index[$v['match_id']];
-                $data = [
-                    "keyword1"=>[
-                        "value"=> "{$match["home"]} vs {$match["away"]}",
-                        "color" => "#FF0000"
-                    ],
-                    "keyword2"=>[
-                        "value"=> $match['match_time'],
-                        "color" => "#173177"
-                    ],
-                    "keyword3"=>[
-                        "value"=> $match['league_name'],
-                        "color" => $match['color']
-                    ]
-                ];
-
-                $log->addDebug("open_id:".json_encode($user['openid']));
-                $log->addDebug("template_id:".$template_id);
-                $log->addDebug("form_id:".$v['form_id']);
-                $log->addDebug("data:".json_encode($data));
-
-                $res = $wxapp->bindRedis(redis())
-                    ->sendTemplateMsg(
-                    $user['openid'],
-                    $template_id,
-                    $v['form_id'],
-                    $data,
-                    "pages/index"
-                );
-                $log->addDebug("res:".$res);
-            }
-        }
+//        $log->addDebug("match_ids:".json_encode($ids));
+//        //增加开始比赛状态改变，做推送
+//        if(empty($ids)||count($ids)<1)
+//        {
+//            return false;
+//        }
+//
+//        $collect = MatchCollectionModel::fetch("*", ["match_id"=>$ids]);
+//
+//        $where2['m.id'] = $ids;
+//        $res = MatchModel::fetch(
+//            $where2,
+//            [
+//                "m.id(match_id)",
+//                "l.gb_short(league_name)",
+//                "l.color(league_color)",
+//                "m.start_time(match_time)",
+//                "h.gb(home)",
+//                "h.flag(home_flag)",
+//                "a.gb(away)",
+//                "a.flag(away_flag)",
+//                "m.home_score",
+//                "m.away_score",
+//                "m.home_red",
+//                "m.away_red",
+//                "m.home_yellow",
+//                "m.away_yellow",
+//                "m.status",
+//                "m.current_minutes",
+//            ]
+//        );
+//
+//        $match_index = [];
+//        foreach ($res as $k => $v){
+//            $res[$k]['match_time'] = date("Y-m-d H:i:s", $v['match_time']);
+//            $res[$k]['is_collect'] = 0;
+//            $match_index[$v['match_id']] = $res[$k];
+//        }
+//
+//        $conf = config()->get("wxapp");
+//        $wxapp = new Wxapp($conf['app_id'], $conf['app_secret']);
+//        $template_id = "CqlL_4sZ3Axfcth7vFd0bREMM0ayWt4loscCk8hhnBQ";
+//        foreach ($collect as $v){
+//            $log->addDebug("用户：{$v['user_id']}，关注了{$v['match_id']}");
+//            $user = UserModel::getUserInfo($v['user_id'], ["openid"]);
+//            if(!empty($v['form_id'])){
+//
+//                $match = $match_index[$v['match_id']];
+//                $data = [
+//                    "keyword1"=>[
+//                        "value"=> "{$match["home"]} vs {$match["away"]}",
+//                        "color" => "#FF0000"
+//                    ],
+//                    "keyword2"=>[
+//                        "value"=> $match['match_time'],
+//                        "color" => "#173177"
+//                    ],
+//                    "keyword3"=>[
+//                        "value"=> $match['league_name'],
+//                        "color" => $match['color']
+//                    ]
+//                ];
+//
+//                $log->addDebug("open_id:".json_encode($user['openid']));
+//                $log->addDebug("template_id:".$template_id);
+//                $log->addDebug("form_id:".$v['form_id']);
+//                $log->addDebug("data:".json_encode($data));
+//
+//                $res = $wxapp->bindRedis(redis())
+//                    ->sendTemplateMsg(
+//                    $user['openid'],
+//                    $template_id,
+//                    $v['form_id'],
+//                    $data,
+//                    "pages/index"
+//                );
+//                $log->addDebug("res:".$res);
+//            }
+//        }
 
         return false;
     }

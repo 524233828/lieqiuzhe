c7cc2397ae794bb8d2743f1cb57931ec9d154b41
JoseChan <chenyu01@linghit.com>
2018-06-19 09:29:59 +0800

    fix

--- config/config.php
+++ config/config.php
@@ -54,9 +54,9 @@ return [
         // 支付宝支付参数
         'alipay' => [
             'debug'       => false, // 沙箱模式
-            'app_id'      => '', // 应用ID
-            'public_key'  => '', // 支付宝公钥(1行填写)
-            'private_key' => '', // 支付宝私钥(1行填写)
+            'app_id'      => '2088821648121441', // 应用ID
+            'public_key'  => 'MIgGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCnxj/9qwVfgoUh/y2W89L6BkRAFljhNhgPdyPuBV64bfQNN1PjbCzkIM6qRdKBoLPXmKKMiFYnkd6rAoprih3/PrQEB/VsW8OoM8fxn67UDYuyBTqA23MML9q1+ilIZwBC2AQ2UBVOrFXfFl75p6/B5KsiNG9zpgmLCUYuLkxpLQIDAQAB', // 支付宝公钥(1行填写)
+            'private_key' => 'MIICWwIBAAKBgQDYDNrZEPOsXdLecCSwKeYDe6VbnBIVzBCWXnCW2y1ZcNVVGoJiW3PtrjghYjxROrOk300jQ7SRXLdeghz3x1vt+az2j/Roy57mC9fCynWQdVcUs4UjLBDWjra8kcjzGYyNYfhPYheTC1nGdhn08DHX0d/6B2o/BxMYTcxVDAzufQIDAQABAoGAEUElkURrUY7EsoMeSvttpUWQtTpHr3n2sSulrkae3o/GWd+eHiDTp13Mmc3wp0Qa6MX0sSZNG3beJiwaCCfhzYANK7iT6aTFClFtDhAr4QRmdFs86vHgJU2HvUu9WxbHywMCI2c8z+DS36sGfWpREN6HQObh5OUUjVwK3liAK7kCQQD44tyVo4EOL9IvtkLIR4hqh4korsoxT3tBOA1iCv3QchRFjAuCO67UimBtTqXP9RhnaYBtuI1p+yNA2eG+CNRzAkEA3jm6OPCjOzMPPUx4BmOUZy4zIFg/BJT8VV4Rt/nh6P/HIjxo+r/cHNm+X1veTDvPL19Rrq/YmNEDyuf25/ZlTwJAQSveiQyEebuJ9VZrjFg233ZYMx/58AmZA83yqy6nodKNflyakuKf+CW39Ed44ciTOFkG+TQvS4YoiA3Fr+ZOAwJAU02CgEu8dLmcMddTetmjTNZlte8+mEIdIQclTzjttYEELdJFbBG1ul7pXSe7+gnFjbWGkhw67nYTnOE9jhCwQwJAQ1H3BPkq4+0ltFcpk782+xc8GEJ0oy47tZuRzpsi1eZp6JSrglwQXlx07UL6IUTWjmOeiHJLwl8uyyuaYrPZ3Q==', // 支付宝私钥(1行填写)
             'notify_url'  => '', // 支付通知URL
         ]
     ],

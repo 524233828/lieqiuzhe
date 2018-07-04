a077878136d2e3dede933213663524bd82966d70
JoseChan <chenyu01@linghit.com>
2018-07-05 00:03:43 +0800

    fix

--- src/Controller/AnalystApplicationController.php
+++ src/Controller/AnalystApplicationController.php
@@ -18,14 +18,14 @@ class AnalystApplicationController extends BaseController
     public function addAnalystApplication(ServerRequest $request)
     {
         $validate = validator($request, [
-            "nickname" => "required|range:1,32",
+            "nickname" => "required|string|range:1,32",
             "sex" => "required|in:男,女",
-            "tag" => "required|range:1,5",
+            "tag" => "required|string|range:1,5",
             "ball_year" => "required|in:1-5年,5-10年,10-20年,20-30年,30年以上,其他",
             "league" => "required|multi_in:英超,西甲,德甲,法甲,意甲,其他",
             "skill" => "required|in:保本盈利,冷门,其他",
             "good_at" => "required|multi_in:欧赔,精彩赔率,亚盘,交锋数据,球队近绩,球员数据,控球率,失球率,球队打法,天气,赛前信息,其他",
-            "intro" => "required"
+            "intro" => "required|string"
         ]);
 
         $data = $validate->data();

5d227d1ddd83ab3609f067dde911b148e53eddb1
zuoan-link <15277712342@163.com>
2018-07-08 15:54:19 +0800

    update

--- src/Model/RecommendModel.php
+++ src/Model/RecommendModel.php
@@ -124,6 +124,7 @@ SQL;
             'm.title as rec_title',
             'm.`desc` as rec_desc',
             'm.`result` as result',
+            "FROM_UNIXTIME(h.start_time,'%m/%d %H:%i') as match_time",
             'm.`option_id` as option_id',
         ];
         $column = is_array($columns) ? implode(",", $columns) : $columns;

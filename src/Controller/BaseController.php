9c440d393ed3d1e6e5a7b211dde3814f5f75faee
JoseChan <chenyu01@linghit.com>
2018-08-20 10:12:03 +0800

    fix

--- src/Controller/BaseController.php
+++ src/Controller/BaseController.php
@@ -36,6 +36,7 @@ class BaseController
     public function response($data, $isFilter = false, $msg = '', $code = ErrorCode::OK, $status = 200)
     {
         $data = $isFilter?$this->responseFilter($data):$data;
+        $data = empty($data)?["list" =>[]]:$data;
         $msg = empty($msg)?(ErrorCode::msg($code) ? ErrorCode::msg($code) : $msg): $msg;
         $status = ErrorCode::status($code) ? ErrorCode::status($code) : $status;
         return new ApiResponse($msg, $code, $data, $status);
